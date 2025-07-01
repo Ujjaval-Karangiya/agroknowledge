ec5371dcddf2450dc20d34f0a7229ea6446021c5
Ujjaval-Karangiya <ujjavalk2401.gmail.com>
2025-07-01 17:59:18 +0530

    Agroknowledge

--- server/server.js
+++ server/server.js
@@ -1,21 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./routes/auth');
 const cors = require('cors');
 const path = require('path');
-
+const authRoutes = require('./routes/auth');
+const adminRoutes = require('./routes/admin');
+app.use('/api/users', adminRoutes);
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
-
-// Routes
 app.use('/api/auth', authRoutes);
 
-// MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => app.listen(process.env.PORT, () => {
-    console.log('Server running on port ' + process.env.PORT);
-  }))
-  .catch(err => console.error(err));
+
+main()
+    .then(() => {
+  console.log('✅ MongoDB Connected');
+  app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
+}).catch(err => {
+  console.error('❌ MongoDB connection error:', err);
+});
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/agro');
+}

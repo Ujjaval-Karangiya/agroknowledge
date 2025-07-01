43da1825bd2d1986ee426fd4d77ac4008385143e
Ujjaval-Karangiya <ujjavalk2401.gmail.com>
2025-07-01 13:18:07 +0530

    AgroStar Auth System

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => app.listen(process.env.PORT, () => {
+    console.log('Server running on port ' + process.env.PORT);
+  }))
+  .catch(err => console.error(err));
